991aeefddf2ecc8b8ad4a1eafaa1f047527ce43c
mateusf-prog <mateus102006@hotmail.com>
2023-11-06 11:07:55 -0300

    novo método na classe Vetor

--- src/vetores/Vetor.java
+++ src/vetores/Vetor.java
@@ -1,5 +1,7 @@
 package vetores;
 
+import java.util.Arrays;
+
 public class Vetor<T> {
 /*
  * Vetor é uma estrutura de dados que armazena uma sequência de elementos
@@ -7,13 +9,42 @@ public class Vetor<T> {
  * tem tamanho fixo, enquanto a lista tem tamanho variável.
  */
     private Object[] elementos;
+    private int posicao;
 
     public Vetor(int tamanho) {
        this.elementos = new Object[tamanho];
     }
+    public Vetor() {
+        this.elementos = new Object[3];
+        this.posicao = 0;
+    }
 
-    public void inserirEm(int posicao, T elementos) {
-        this.elementos[posicao] = elementos;
+    public void inserir(T elemento) {
+        if (this.posicao >= this.elementos.length) {
+            this.elementos = Arrays.copyOf(this.elementos, this.elementos.length + 1);
+        }
+        this.elementos[this.posicao] = elemento;
+    }
+
+    public void inserirEm(int posicao, T elemento) {
+        if (posicao > this.elementos.length) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        
+        if (this.elementos != null) {
+            Object[] arrayFinal = Arrays.copyOfRange(this.elementos, posicao, this.elementos.length);
+            Object[] arrayInicio = new Object[posicao + 1];
+            System.arraycopy(this.elementos, 0, arrayInicio, 0, posicao);
+            arrayInicio[arrayInicio.length - 1] = elemento;
+            int novoTamanho = arrayInicio.length + arrayFinal.length;
+            this.elementos = new Object[novoTamanho];
+            System.arraycopy(arrayInicio, 0, this.elementos, 0, arrayInicio.length);
+            System.arraycopy(arrayFinal, 0, this.elementos, arrayInicio.length, arrayFinal.length);
+
+        } else {
+            this.elementos[posicao] = elementos;
+        }
+        
     }
 
     @SuppressWarnings("unchecked") // ignora o warning de cast 
