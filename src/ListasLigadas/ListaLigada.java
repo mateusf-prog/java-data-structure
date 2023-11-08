49a69d214eba554134fc8befce6f6e6f16073700
mateusf-prog <mateus102006@hotmail.com>
2023-11-08 11:49:18 -0300

    metodo inserir - LinkedList

--- src/ListasLigadas/ListaLigada.java
+++ src/ListasLigadas/ListaLigada.java
@@ -11,8 +11,23 @@ public class ListaLigada<T> {
         this.ultimoNo = null;
     }
 
-    
+    // método para inserir um elemento na lista
+    public void inserir(T elemento) {
+        No<T> novoNo = new No<T>(elemento);    // iniciando a lista
+        if (estaVazia()) {                     // se a lista estiver vazia
+            this.primeiroNo = novoNo;          // o primeiro nó será o novo nó
+            this.ultimoNo = novoNo;            // o último nó será o novo nó
+        } else {
+            this.ultimoNo.setProximo(novoNo);  // inserindo no final da lista
+        }
+        this.tamanho++;
+    } 
 
+    public boolean estaVazia() {
+        return this.tamanho == 0;
+    }
 
-    
+    public int tamanho() {
+        return this.tamanho;
+    }
 }
